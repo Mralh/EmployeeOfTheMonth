5a2d7f2820674b2eef99117d2f726412fa55f8fb
Mralh <mralhamm@gmail.com>
2017-04-10 12:25:02 -0400

    minigame api pt1

---
+++ Assets/Scripts/MiniGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame : MonoBehaviour {
+
+    public int timeLimit;
+    public int timer = 0;
+    public int scoreRequired = 0;
+
+    int playerScores = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    public virtual void Tick() { }
+    public virtual void OnGameStart()
+    {
+    }
+    public virtual void OnGameEnd()
+    {
+    }
+
+    public void ChangeScore(int playerID, int score)
+    {
+        playerScores += score;
+    }
+    
+    public void ResetScore(int playerID)
+    {
+        playerScores = 0;
+    }
+}
