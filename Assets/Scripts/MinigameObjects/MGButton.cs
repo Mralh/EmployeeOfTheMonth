ffeb0cbcc1c62a6d8ef297734f14617032f5c4a3
Mralh <mralhamm@gmail.com>
2017-05-02 21:48:46 -0400

    pOP QUIZ

--- Assets/Scripts/MinigameObjects/MGButton.cs
+++ Assets/Scripts/MinigameObjects/MGButton.cs
@@ -6,11 +6,31 @@ public class MGButton : MonoBehaviour
 {
     public Vector3 openPosition, closedPosition;
 
+    public bool localSpace;
+
+    Vector3 initialPos;
+
+    void Start ()
+    {
+        initialPos = transform.localPosition;
+    }
     void FixedUpdate ()
     {
         if (GetComponent<MiniGameObject>().interact)
-            transform.localPosition = Vector3.Lerp(transform.localPosition, closedPosition, 10f * Time.deltaTime);
+        {
+            if (!localSpace)
+                transform.localPosition = Vector3.Lerp(transform.localPosition, closedPosition, 10f * Time.deltaTime);
+            else
+                transform.localPosition = Vector3.Lerp(transform.localPosition, initialPos + 
+                    closedPosition.x*transform.right + closedPosition.y*transform.up + closedPosition.z*transform.forward,
+                    10f * Time.deltaTime);
+        }
         else
-            transform.localPosition = Vector3.Lerp(transform.localPosition, openPosition, 10f * Time.deltaTime);
+        {
+            if (!localSpace)
+                transform.localPosition = Vector3.Lerp(transform.localPosition, openPosition, 10f * Time.deltaTime);
+            else
+                transform.localPosition = Vector3.Lerp(transform.localPosition, initialPos, 10f * Time.deltaTime);
+        }
     }
 }
---
+++ Assets/Scripts/Minigames/PopQuiz.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopQuiz : MiniGame {
+
+    List<Transform> spawnPoints;
+    string prefabPath = "Prefabs/Objects/PopQuiz/";
+    GameObject bomb;
+
+    public PopQuiz(MiniGameManager mg) : base(mg) { }
+
+    public override void ScenePrewarm()
+    {
+        base.ScenePrewarm();
+        base.manager.RequestSceneChange("darkroom");
+    }
+
+    public override void OnTransitionIn()
+    {
+        base.OnTransitionIn();
+        manager.player.playBGM("quiztime");
+
+        base.scoreRequired = 1;
+        //Timers
+        base.startTimeLimit = 2 * 60;
+        base.endTimeLimit = 3 * 60;
+        base.timeLimit = (int)((float)(7 * 60) / manager.speedModifier);
+
+        //Messages
+        base.introMessages = new string[] { "POP QUIZ" };
+        base.failureMessages = new string[] { "Wrong you dummy", "Don't you go to Purdue?", "Not even close" };
+        base.successMessages = new string[] { "WOW good answer!", "You can did it", "CORRECT" };
+
+        //Base Game Objects + Lightpacks
+        GameObject objectPack = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "ObjectPack"));
+        objectPack.name = "ObjectPack";
+        base.loadedObjects.Add(objectPack);
+
+        GameObject quiz = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "QUIZ" + Random.Range(1, 1)));
+        base.loadedObjects.Add(quiz);
+
+
+        manager.SetPlayerPosition(new Vector3(0.36f, 0.0564f, 3.893f), 0);
+    }
+
+    public override void OnGameStart()
+    {
+        base.OnGameStart();
+    }
+    public override void OnGameEnd()
+    {
+        base.OnGameEnd();
+    }
+
+    public override void OnTransitionOut()
+    {
+        base.OnTransitionOut();
+        manager.signalLoad();
+        manager.SelectNextGame();
+    }
+
+    public override void Tick()
+    {
+        if (AllDone())
+            manager.forceEndMinigame();
+        if (playerScores < 0)
+            manager.forceEndMinigame();
+    }
+}
