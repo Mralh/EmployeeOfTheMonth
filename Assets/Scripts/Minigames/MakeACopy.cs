3e2d19d9b8d40825392b25fa847c8428310cf93d
Unknown <gyogan@purdue.edu>
2017-05-02 22:23:48 -0400

    Added Button Object Info

--- Assets/Scripts/Minigames/MakeACopy.cs
+++ Assets/Scripts/Minigames/MakeACopy.cs
@@ -55,24 +55,35 @@ public class MakeACopy : MiniGame {
         //Add the score update to the correct button
         GameObject Button1 = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button1"));
         Button1.AddComponent<AddScoreOnInteract>();
+        Button1.GetComponent<AddScoreOnInteract>().score = 1;
+        base.loadedObjects.Add(Button1);
+
+        GameObject Button2 = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button2"));
+        Button2.AddComponent<AddScoreOnInteract>();
+        Button1.GetComponent<AddScoreOnInteract>().score = -1;
+        base.loadedObjects.Add(Button1);
+
+        GameObject Button3 = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button3"));
+        Button3.AddComponent<AddScoreOnInteract>();
+        Button3.GetComponent<AddScoreOnInteract>().score = -1;
         base.loadedObjects.Add(Button1);
 
         //Need to set all the texts of the buttons here
 
-        for (int j = 0; j < 3; j++)
-        {
+        GameObject Button1Text = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button1Text"));
+        //Button1Text.AddComponent<Text>(selectedButtonTexts.Remove());
+        base.loadedObjects.Add(Button1Text);
 
-            GameObject Button1Text = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button1Text"));
-            //Button1Text.AddComponent<Text>(selectedButtonTexts.Remove());
-            base.loadedObjects.Add(Button1Text);
+        GameObject Button2Text = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button2Text"));
+        //Button1Text.AddComponent<Text>(selectedButtonTexts.Remove());
+        base.loadedObjects.Add(Button2Text);
 
-        }
+        GameObject Button3Text = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "Button3Text"));
+        //Button1Text.AddComponent<Text>(selectedButtonTexts.Remove());
+        base.loadedObjects.Add(Button3Text);
+
 
         manager.SetPlayerPosition(new Vector3(-3.565f, 0f, -6.386f), 0);
-
-        /*GameObject phone = GameObject.Instantiate(Resources.Load<GameObject>(prefabPath + "walruS7"), new Vector3(-3.85f, 2f, -2.124f), Quaternion.identity);
-        loadedObjects.Add(phone);
-        phone.GetComponent<Phone>().trigger = trigger;*/
 
     }
 
@@ -91,7 +102,9 @@ public class MakeACopy : MiniGame {
     {
         if (AllDone())
             manager.forceEndMinigame();
-    }
+        if (playerScores < 0)
+            manager.forceEndMinigame();
+}
 
     // Use this for initialization
     void Start () {
